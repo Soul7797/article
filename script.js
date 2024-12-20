d550c956343b42f4dbbfcc964b7686d53bb8ed33
Soul <160653838+Soul7797@users.noreply.github.com>
2024-12-19 20:27:08 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Intersection Observer to reveal sections
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    }
+  );
+
+  document.querySelectorAll(".reveal").forEach((sec) => observer.observe(sec));
+});
